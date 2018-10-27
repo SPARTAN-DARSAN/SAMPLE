ec358f34b9c60f0a3f0d80bca64ae8e5a74f738e
SPARTAN-DARSAN <44530652+SPARTAN-DARSAN@users.noreply.github.com>
2018-10-27 23:06:35 +0530

    Create NEW.c

---
+++ NEW.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c=a+b;
+clrscr();
+printf("enter any 2 number"\n);
+scanf("%d%d",&a,&b);
+printf("answer is %d",&c);
+getch();
+return(a+b);
+}
